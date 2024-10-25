1b00f3aa3bff33bc25c6f73b6ec9318f575b00a7
Razin Ares <root@razin.me>
2024-10-26 01:35:16 +0600

    Init Keys
    Signed-off-by: Razin Ares <root@razin.me>

--- make_key.sh
+++ make_key.sh
@@ -6,4 +6,5 @@
 set -u
 bash <(sed "s/2048/${2:-2048}/;/Enter password/,+1d" ../../../development/tools/make_key) \
     $1 \
-    '/C=US/ST=California/L=Mountain View/O=Android/OU=Android/CN=Android/emailAddress=android@android.com'
+    '/C=BD/ST=Dhaka/L=Savar/O=RAZIN/OU=RAZIN/CN=RAZIN/emailAddress=root@razin.me'
+
